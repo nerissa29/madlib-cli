35de3a189ce4e914b8b0da0f67d303735486132e
Nerissa Leynes <leynes.nerissa@gmail.com>
2022-12-21 22:09:57 -0500

    updated madlib.py, created list

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -10,59 +10,15 @@ Have fun!
 
 user_response_list = []
 
+madlib = ["adjective1", "adjective2", "adjective3", "adjective4", "adjective5",  >>> 4
+          "adjective6", "First Name1", "First Name2", "First Name3", "past tense", >>> 5-9
+          "plural noun1", "plural noun2", "plural noun3", "plural noun4",   >>> 10-13
+          "plural noun5", "large animal", "small animal", "Girl's name", "number (1-50)",  >>> 14-18
+          "number (1-50)", "number (1-50)"]
+for index, item in enumerate(madlib):
+    user_response_list.append(input(f"Enter {madlib[index]}: "))
 
-adjective1 = input("Enter Adjective: ")
-user_response_list.append(adjective1)
-
-adjective2= input("Enter another adjective: ")
-user_response_list.append(adjective2)
-
-first_name1 = input("Enter First Name: ")
-user_response_list.append(first_name1)
-
-past_tense = input("Enter Past Tense Verb: ")
-user_response_list.append(past_tense)
-
-first_name2 = input("Enter another First Name: ")
-user_response_list.append(first_name2)
-
-adjective3 = input("Enter another Adjective: ")
-user_response_list.append(adjective3)
-
-adjective4 = input("Enter another Adjective: ")
-user_response_list.append(adjective4)
-
-plural = input("Enter Plural Noun: ")
-user_response_list.append(plural)
-
-# plural = input("Enter Large Animal: ")
-#
-# plural = input("Enter Small Animal: ")
-# 
-# plural = input("Enter a Girl's Name: ")
-#
-# adjective5 = input("Enter another Adjective: ")
-#
-# plural2 = input("Enter Plural Noun: ")
-#
-# adjective5 = input("Enter another Adjective: ")
-#
-# plural2 = input("Enter Plural Noun: ")
-#
-# plural2 = input("Enter a number (1-50): ")
-#
-# plural2 = input("Enter First Name: ")
-#
-# plural2 = input("Enter a number: ")
-#
-# plural2 = input("Enter Plural Noun: ")
-#
-# plural2 = input("Enter a number: ")
-#
-# plural2 = input("Enter Plural Noun: ")
-
-
-
+print(user_response_list)
 
 """
 I the {Adjective} and {Adjective} {A First Name} have 
@@ -79,13 +35,22 @@ along with hundreds of other goodies for you to find.
 """
 # read in the story.txt file
 # with open("story.txt", "w") as f:
-#     f.write(f"I the {user_response_list[0]} and {user_response_list[1]} {user_response_list[2]} "
-#             f"have {user_response_list[3]} {user_response_list[4]}'s {user_response_list[5]} sister "
-#             f"and plan to steal her {user_response_list[6]} {user_response_list[7]}! \n"
-#             f"What are a {Large Animal} and backpacking {Small Animal} ")
+#     f.write(f"I the {user_response_list[0]} and {user_response_list[1]} {user_response_list[6]} "
+#             f"have {user_response_list[9]} {user_response_list[7]}'s {user_response_list[2]} sister "
+#             f"and plan to steal her {user_response_list[3]} {user_response_list[11]}! \n"
+#             f"What are a {user_response_list[15]} and backpacking {user_response_list[16]} "
+#             f"to do? Before you can help {user_response_list[17]}, you'll have to "
+#             f"collect the {user_response_list[4]} {user_response_list[12]} and {user_response_list[13]} {user_response_list[14]} "
+#             f"that open up the {user_response_list[18]} worlds connected to A {user_response_list[8]}'s Lair. "
+
+#             f"There are {user_response_list[19]} {user_response_list[13]} and {user_response_list[20]} {user_response_list[14]} in the game, "
+#             f"along with hundreds of other goodies for you to find.")
 
 
-print(user_response_list)
+with open("story.txt", 'r') as  f:
+    contents = f.read()
+
+# print(contents)
 
 
 
